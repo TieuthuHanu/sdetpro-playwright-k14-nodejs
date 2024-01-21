aa5538fb495a133985096f9f2e9baea816ef4fd1
NHU VO <Nhu Vo>
2024-01-20 23:06:42 -0800

    Calculate BMI

--- labs/lab_02/Lab2_1.js
+++ labs/lab_02/Lab2_1.js
@@ -4,9 +4,9 @@ let BMI = weight / (height * 2);
 
 if (BMI <= 18.5)
     console.log("You're underweight");
-else if (BMI < 25)
+else if (BMI <= 24.9)
     console.log("You're normal");
-else if (BMI < 30)
+else if (BMI <= 29.9)
     console.log("You're overweight");
 else
     console.log("You're OBESITY!!!");

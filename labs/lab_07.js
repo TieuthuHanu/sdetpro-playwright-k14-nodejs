59708255fed728c71baf34f386b9680a6618fb91
NHU VO <Nhu Vo>
2024-02-25 09:46:26 -0800

    countWords in the session by using object, not a Map (do NOT use Object.entries() method to get a map)

---
+++ labs/lab_07.js
@@ -0,0 +1,16 @@
+const readlineSync = require('readline-sync');
+groupWords();
+
+function groupWords() {
+    const groupedWords = {};
+    const userInput = readlineSync.question('Your sentence: ');
+    const words = userInput.split(' ');
+    words.forEach(function (word) {
+        if(groupedWords[word]) {
+            groupedWords[word]++;
+        } else {
+            groupedWords[word] = 1;
+        }
+    });
+    console.log(groupedWords);
+}
